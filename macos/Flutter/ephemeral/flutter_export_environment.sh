569388e17e386064c582d67263f5d019ed593506
Mugilarasan <your.email@example.com>
2025-11-14 13:04:35 +0530

    Update Podfile.lock to replace app_links with camera_avfoundation; modify Info.plist for insecure HTTP loads; set DEVELOPMENT_TEAM in project.pbxproj; update Flutter environment paths in generated files.

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/brito.jaison88gb.in/develop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/brito.jaison88gb.in/brainleap/frontend"
+export "FLUTTER_ROOT=/Users/mugilarasan/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mugilarasan/88GB Tech/brain_leap/brainleap-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
